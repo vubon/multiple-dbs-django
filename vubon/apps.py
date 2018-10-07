9054ec144637c8f6af74a41503ba9dd93d2bf4c2
vubon <vubon.roy@gmail.com>
2018-10-07 17:27:14 +0600

    update gitignore

--- vubon/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VubonConfig(AppConfig):
-    name = 'vubon'
--- vubon/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import GetAndCreate
-
-urlpatterns = [
-    url(r'^get-create/', GetAndCreate.as_view(), name='get-create'),
-]
